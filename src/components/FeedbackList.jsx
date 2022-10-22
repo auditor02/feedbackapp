bee650994038b3a83b223ffabc4160b0e394f962
Ekomobong Wisdom <wisdomn200@gmail.com>
2022-10-22 22:35:03 +0100

    this is now a fullstack project with a backend built with json

--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -2,18 +2,19 @@ import { useContext } from 'react'
 import {motion, AnimatePresence} from 'framer-motion'
 import FeedbackItem from "./FeedbackItem"
 import FeedbackContext from './context/FeedbackContext'
-
+import Spinner from './shared/Spinner'
 
 
 function FeedbackList() {
-  const { feedback } = useContext(FeedbackContext)
+  const { feedback, isLoading } = useContext(FeedbackContext)
 
-    if (!feedback || feedback.length === 0) {
+    if (!isLoading && (!feedback || feedback.length === 0)) {
         return <p>No Feedback Yet</p>
     }
 
-
-    return (
+    return isLoading ? (
+      <Spinner />
+    ): (
       <div className='feedback-list'>
         <AnimatePresence>
         {feedback.map((item) => (
@@ -32,6 +33,7 @@ function FeedbackList() {
         </AnimatePresence>
       </div>
     )
+
 
   // return (
   //   <div className='feedback-list'>
